849c33e378db955d691869bb4d86842f2551a0bd
Juan Diego S. <jdsaquico@hotmail.com>
2026-05-12 02:57:41 +0200

    Add page interactions

---
+++ script.js
@@ -0,0 +1,21 @@
+const yearElement = document.querySelector('#year');
+const navToggle = document.querySelector('.nav-toggle');
+const siteNav = document.querySelector('.site-nav');
+
+if (yearElement) {
+  yearElement.textContent = new Date().getFullYear();
+}
+
+if (navToggle && siteNav) {
+  navToggle.addEventListener('click', () => {
+    const isOpen = siteNav.classList.toggle('is-open');
+    navToggle.setAttribute('aria-expanded', String(isOpen));
+  });
+
+  siteNav.querySelectorAll('a').forEach((link) => {
+    link.addEventListener('click', () => {
+      siteNav.classList.remove('is-open');
+      navToggle.setAttribute('aria-expanded', 'false');
+    });
+  });
+}
